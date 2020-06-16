44f0dfa1322586707b51ebde9235223a27515066
Narbis Tamiche <54629185+NarbisCOD@users.noreply.github.com>
2020-06-16 09:04:55 -0300

    Update Ejercicio2F.c

--- Ejercicio2F.c
+++ Ejercicio2F.c
@@ -2,26 +2,37 @@
 
 #define OPC_UNO 1
 #define OPC_DOS 2
+#define MENJ_ERROR ("----------/--------\nOpcion Invalida, verifique las opciones disponibles\n\n")
+#define MENJ_OPC_UNO ("1.-Quiero aprender a Programar\n")
+#define MENJ_OPC_DOS ("2.-Quiero ser experto en programando\n")
+#define MENJ_RESUL_UNO ("Ya empezaste, te felicito\n")
+#define MENJ_RESUL_DOS ("Practica, practica, practica...ah y apoyate con docentes e internet\n")
+
+
 
 const int DEF_OPCION = OPC_DOS;
 
 void mostrar_mensaje(int opcion_evaluar){
 	switch(opcion_evaluar){
 		case OPC_UNO:
-			printf("Ya empezaste, te felicito\n");
+			printf(MENJ_RESUL_UNO );
 			break;
 			default:
-			printf("Practica, practica, practica...ah y apoyate con docentes e internet\n");
+			printf(MENJ_RESUL_DOS);
 	}
 }
 
 int elegir_opcion_menu(int opcion_actual){
     do{
-	    printf("1.-Quiero aprender a Programar\n");
-		printf("2.-Quiero ser experto en programando\n");
+	    printf(MENJ_OPC_UNO);
+		printf(MENJ_OPC_DOS);
         printf("Ingrese una Opcion:\n");
-
 		scanf("%i", &opcion_actual);
+
+		if((opcion_actual> OPC_UNO)|| (opcion_actual> OPC_DOS)){
+			printf(MENJ_ERROR);
+		}
+		
 	}while(opcion_actual> DEF_OPCION);
       
 	return opcion_actual;
@@ -33,3 +44,4 @@ int main(){
 	mostrar_mensaje(opcion);
 	return 0;
 }
+
